c0dcab2c1932cd7f7c237a59a1998acca8090009
Gustavo Marques <gustavo.markez23@gmail.com>
2022-02-20 21:45:12 -0300

    Criando ações por linha de comando

---
+++ cli.js
@@ -0,0 +1,4 @@
+import pegarArquivo from './index.js';
+
+const caminho = process.argv;
+pegarArquivo(process.argv[2]);
--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-console.log('Vamos começar');
-let paragrafo = 'Texto retornado por uma função';
-
-function texto(string){
-  return string;
+function extraiLink(text){
+  const regex = /\[([^\]]*)\]\((http?s:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while((temp = regex.exec(text)) != null){
+    arrayResultados.push({[temp[1]]: temp[2]});
+  }
+  return arrayResultados;
 }
 
-console.log(chalk.blue(texto(paragrafo)));
+function trataErro(erro){
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+export default async function pegarArquivo(caminhoDoArquivo){
+  const encoding = 'utf-8';
+  try{
+    const text = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extraiLink(text));
+  }catch(erro){
+   trataErro(erro); 
+  }
+}
+
+// pegarArquivo('./arquivo/texto1.md');
+
+// function pegarArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.promises.readFile(caminhoDoArquivo, encoding)
+//   .then((text) => console.log(chalk.green(text)))
+//   .catch((erro) => trataErro(erro));
+// }
+
+// function pegarArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, text) => {
+//     if(erro){
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(text));
+//   })
+// }
