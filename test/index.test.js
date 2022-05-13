902609470ffed3ab53ae23fded048b0cf4b7aff0
Gustavo Marques <gustavo.markez23@gmail.com>
2022-05-13 10:34:35 -0300

    Ajustando teste

--- test/index.test.js
+++ test/index.test.js
@@ -11,12 +11,11 @@ describe('pegarArquivo function', () => {
     expect(typeof pegarArquivo).toBe('function');
   });
   it('deve retornar array de resultados', async () => {
-    // const resultado = await pegarArquivo('D:\Documents\GitHub\node-lib-markdown\test\files\texto1.md');
-    const resultado = await pegarArquivo('test\files\texto1.md');
+    const resultado = await pegarArquivo('./test/files/texto1.md');
     expect(resultado).toEqual(arrayResult);
   });
-  // it('Deve retornar a mensagem "não há links"', async () => {
-  //   const result = await pegarArquivo('D:\Documents\GitHub\node-lib-markdown\test\files\texto1.md')
-  //   expect(result).toBe('Sem links');
-  // });
+  it('Deve retornar a mensagem "não há links"', async () => {
+    const result = await pegarArquivo('./test/files/texto1_without_link.md')
+    expect(result).toBe('Não há links');
+  });
 });
